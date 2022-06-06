d497b0935ec45026f38e2f4270fa7ad2abd8d4d8
71walceli <antonini2903@hotmail.com>
2022-06-06 18:57:25 -0500

    Req. 1: Escaneo de códigos QR
    - Actividad de escaneo de códigos QR
    - Avance en componente `RegistroJornalero`

---
+++ src/ClienteMovil/app/src/main/java/com/example/controlasistenciafincalolita/clientemovil/Intro.kt
@@ -0,0 +1,53 @@
+package com.example.controlasistenciafincalolita.clientemovil
+
+import android.app.SearchManager
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import com.google.zxing.integration.android.IntentIntegrator
+
+class Intro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        val bienvenidoUsuario: TextView = findViewById(R.id.bienvendioUsuario)
+        bienvenidoUsuario.text = resources.getString(R.string.bienvenidoUsuario, "Walter Celi")
+        val qrButton: Button = findViewById(R.id.qr_button)
+
+        IniciarEscaneoQr()
+
+        qrButton.setOnClickListener {
+            IniciarEscaneoQr()
+        }
+    }
+
+    private fun IniciarEscaneoQr() {
+        val intentIntegrator = IntentIntegrator(this)
+        intentIntegrator.setDesiredBarcodeFormats(listOf(IntentIntegrator.QR_CODE))
+        intentIntegrator.setCaptureActivity(EscaneoCodigoQr::class.java)
+        intentIntegrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        var result = IntentIntegrator.parseActivityResult(resultCode, data)
+        if (result != null && result.contents != null) {
+            AlertDialog.Builder(this)
+                .setMessage("Would you like to go to ${result.contents}?")
+                .setPositiveButton("Yes", DialogInterface.OnClickListener { dialogInterface, i ->
+                    val intent = Intent(Intent.ACTION_WEB_SEARCH)
+                    intent.putExtra(SearchManager.QUERY,result.contents)
+                    startActivity(intent)
+                })
+                .setNegativeButton("No", DialogInterface.OnClickListener { dialogInterface, i ->  })
+                .create()
+                .show()
+
+        }
+    }
+}
