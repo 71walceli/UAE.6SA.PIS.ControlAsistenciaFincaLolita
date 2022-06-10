af6c60bc899b7ca0b23c9c47de313baa2904540a
71walceli <antonini2903@hotmail.com>
2022-06-09 21:31:57 -0500

    Req. 1: Definiión de actividad para módulo EscaneoCodigoQR

--- src/ClienteMovil/app/src/main/java/com/example/controlasistenciafincalolita/clientemovil/Intro.kt
+++ src/ClienteMovil/app/src/main/java/com/example/controlasistenciafincalolita/clientemovil/Intro.kt
@@ -11,25 +11,38 @@ import androidx.appcompat.app.AlertDialog
 import com.google.zxing.integration.android.IntentIntegrator
 
 class Intro : AppCompatActivity() {
+    var estado: Boolean? = null
+        set(value) {
+            // TODO Cambiar color de fondo según estado...
+            if (value == true) {
+                findViewById<TextView>(R.id.iconoCorrectoError).text = "\uf00c"
+            } else if (value == false) {
+                findViewById<TextView>(R.id.iconoCorrectoError).text = "\uf00d"
+            } else {
+                findViewById<TextView>(R.id.iconoCorrectoError).text = "\uf013"
+            }
+            field = value
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        val bienvenidoUsuario: TextView = findViewById(R.id.bienvendioUsuario)
-        bienvenidoUsuario.text = resources.getString(R.string.bienvenidoUsuario, "Walter Celi")
-        val qrButton: Button = findViewById(R.id.qr_button)
-
+        findViewById<TextView>(R.id.bienvendioUsuario).text = resources.getString(R.string.bienvenidoUsuario, "Walter Celi")
         IniciarEscaneoQr()
 
-        qrButton.setOnClickListener {
+        findViewById<Button>(R.id.botonEscanierQr).setOnClickListener {
             IniciarEscaneoQr()
         }
+
+        estado = null;
     }
 
     private fun IniciarEscaneoQr() {
         val intentIntegrator = IntentIntegrator(this)
         intentIntegrator.setDesiredBarcodeFormats(listOf(IntentIntegrator.QR_CODE))
         intentIntegrator.setCaptureActivity(EscaneoCodigoQr::class.java)
+        intentIntegrator.setPrompt("Apunte la cámara hacia el código QR.")
         intentIntegrator.initiateScan()
     }
 
