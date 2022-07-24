cad9113ab06b1d1dc80c9a54e49fe47bd3c66f6f
71walceli <antonini2903@hotmail.com>
2022-07-24 13:27:58 -0500

    Refactorizaciones

--- src/AdminWeb/admin-web-react/src/ultil/index.js
+++ src/AdminWeb/admin-web-react/src/ultil/index.js
@@ -1,41 +1,43 @@
 
-const API_ROOT = "http://10.255.255.241/api/";
+export const API_ROOT = "http://10.255.255.241/api/";
 
+{/* TODO Convertir en React Hook */ }
 export const hacerLlamadaApiInterna = async (metodo, consulta, json) => {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
 
-    var raw = JSON.stringify(json);
+  var raw = JSON.stringify(json);
 
-    var requestOptions = {
-        method: metodo,
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
+  var requestOptions = {
+    method: metodo,
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
 
-    return await fetch(`${API_ROOT}${consulta}`, requestOptions)
-        .then(response => response.text())
-        .then(result => JSON.parse(result))
-        .catch(error => { throw error });
+  return await fetch(`${API_ROOT}${consulta}`, requestOptions)
+    .then(response => response.text())
+    .then(result => JSON.parse(result))
+    .catch(error => { throw error });
 }
 
+{/* TODO Convertir en React Hook */ }
 const recuperarCredenciales = () => {
-    const _credenciales = JSON.parse(localStorage.getItem("credenciales"))
-    if (!_credenciales)
-      return
-    if (Date.now() >= new Date(_credenciales.expiracion)) {
-      localStorage.removeItem("credenciales")
-    } 
-    return _credenciales
+  const _credenciales = JSON.parse(localStorage.getItem("credenciales"))
+  if (!_credenciales)
+    return
+  if (Date.now() >= new Date(_credenciales.expiracion)) {
+    localStorage.removeItem("credenciales")
+  }
+  return _credenciales
 }
 
 // Obtenidio de https://stackoverflow.com/a/48125735/7107690
 export function esperar(time) {
   return new Promise(resolve => {
-      setTimeout(() => {
-          resolve();
-      }, time);
+    setTimeout(() => {
+      resolve();
+    }, time);
   });
 }
 
